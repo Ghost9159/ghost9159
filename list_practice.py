107d87a76ae23901b3e057d462f808c7edcdec04
Ghost9159 <jpetersen1168@gmail.com>
2015-01-09 09:19:53 -0800

    Create list_practice.py

---
+++ list_practice.py
@@ -0,0 +1,16 @@
+#Filename: list_practice.py
+#Author: ghost9159
+#Date: 1/9/15
+
+#vaiables
+list = ["cats", "dogs", "humans", "rhinos", "scissors", "penguins", "tigers"]
+print "In the zoo he have..."
+
+#loop
+for animal in list:    
+    if animal == "scissors":
+        print "Thats not an animal!"
+        break   #stops code if item in list if scissors
+    print f
+else: 
+    print "It's a great zoo!"
